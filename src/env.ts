92ea3d19ee2c6170672ae31d7890a88dc9ae3f59
Özgür Akkurt <oezgurmakkurt@gmail.com>
2022-04-19 18:50:17 +0300

    add defaults to env vars

--- src/env.ts
+++ src/env.ts
@@ -4,10 +4,10 @@ export const ipfsReadOnlyNodeUrl = process.env.IPFS_READ_ONLY_NODE_URL || 'http:
 
 export const port = process.env.OFFCHAIN_SERVER_PORT || 3001
 
-export const dbName = process.env.DB_NAME
-export const dbUser = process.env.DB_USER
-export const dbPass = process.env.DB_PASS
-export const dbHost = process.env.DB_HOST
+export const dbName = process.env.DB_NAME || "subsocial";
+export const dbUser = process.env.DB_USER || "dev";
+export const dbPass = process.env.DB_PASS || "1986";
+export const dbHost = process.env.DB_HOST || "localhost";
 export const dbPort = parseInt(process.env.DB_PORT || '') || 5432
 export const batchSize = process.env.BATCH_SIZE ? parseInt(process.env.BATCH_SIZE) : 500;
-export const chainNode = process.env.CHAIN_NODE;
+export const chainNode = process.env.CHAIN_NODE || "wss://arch.subsocial.network";
