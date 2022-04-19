47025e0efaf172a2dad099fcf28a1a5fd32000d1
Özgür Akkurt <oezgurmakkurt@gmail.com>
2022-04-19 19:24:19 +0300

    fix subsocial urls

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-export const ipfsReadOnlyNodeUrl = process.env.IPFS_READ_ONLY_NODE_URL || 'https://app.subsocial/network/ipfs'
+export const ipfsReadOnlyNodeUrl = process.env.IPFS_READ_ONLY_NODE_URL || 'https://app.subsocial.network/ipfs'
 
 export const dbName = process.env.DB_NAME || "subsocial";
 export const dbUser = process.env.DB_USER || "dev";
@@ -9,4 +9,4 @@ export const dbHost = process.env.DB_HOST || "localhost";
 export const dbPort = parseInt(process.env.DB_PORT || '') || 5432
 export const batchSize = process.env.BATCH_SIZE ? parseInt(process.env.BATCH_SIZE) : 500;
 export const chainNode = process.env.CHAIN_NODE || "wss://arch.subsocial.network";
-export const offchainUrl = process.env.OFFCHAIN_URL || "https://app.subsocial/network/offchain";
+export const offchainUrl = process.env.OFFCHAIN_URL || "https://app.subsocial.network/network/offchain";
